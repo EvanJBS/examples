1e6cefb5e6faea8dfe2f0b99eeec3cff716aa1e5
RustyRaptor <anonymous0programmer@gmail.com>
2017-11-29 16:46:12 -0700

    Reformatted Again for comments (also studied code)

--- mecanum-drive/robot.py
+++ mecanum-drive/robot.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-'''
+"""
     This is a demo program showing how to use Mecanum control with the
     RobotDrive class.
-'''
+"""
 
 import wpilib
 from wpilib import RobotDrive
@@ -16,10 +16,10 @@ class MyRobot(wpilib.SampleRobot):
     rearRightChannel = 0
 
     # The channel on the driver station that the joystick is connected to
-    joystickChannel = 0;
+    joystickChannel = 0
 
     def robotInit(self):
-        '''Robot initialization function'''
+        """Robot initialization function"""
 
         self.robotDrive = wpilib.RobotDrive(self.frontLeftChannel,
                                             self.rearLeftChannel,
@@ -37,7 +37,7 @@ class MyRobot(wpilib.SampleRobot):
         self.stick = wpilib.Joystick(self.joystickChannel)
 
     def operatorControl(self):
-        '''Runs the motors with Mecanum drive.'''
+        """Runs the motors with Mecanum drive."""
 
         self.robotDrive.setSafetyEnabled(True)
         while self.isOperatorControl() and self.isEnabled():
@@ -45,7 +45,7 @@ class MyRobot(wpilib.SampleRobot):
             # This sample does not use field-oriented drive, so the gyro input is set to zero.
             self.robotDrive.mecanumDrive_Cartesian(self.stick.getX(),
                                                    self.stick.getY(),
-                                                   self.stick.getZ(), 0);
+                                                   self.stick.getZ(), 0)
 
             wpilib.Timer.delay(0.005)  # wait 5ms to avoid hogging CPU cycles
 
